42174b1f4215bd72ced3b6fdd907a280d7643631
szabolcshlatky <szabolcshlatky@gmail.com>
2023-04-27 14:41:14 +0200

    MongoDB uri has changed.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://antonio:antonio@rest-api.42iqssw.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://szabolcshlatky:JDcbnK3G9x5fSbjW@rest-api.42iqssw.mongodb.net/?retryWrites=true&w=majority";
 
 /* Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
