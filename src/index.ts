16654c1c4fd73d0991bb9512b0eb9deea5cca20e
szabolcshlatky <szabolcshlatky@gmail.com>
2023-04-27 14:38:36 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://antonio:antonio@rest-api.42iqssw.mongodb.net/?retryWrites=true&w=majority";
+
+/* Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+*/
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log(`Server running on http://localhost:8080/`);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(uri);
+mongoose.connection.on(`error`, (error: Error) => console.log(error));
+
+app.use('/', router());
